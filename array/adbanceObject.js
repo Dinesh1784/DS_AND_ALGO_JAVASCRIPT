54d4232609c61f5c7edcf2d650cf4e9b190e8b8a
allotey3 <d4dinesh52@gmail.com>
2021-11-06 11:46:39 +0530

    day1

---
+++ array/adbanceObject.js
@@ -0,0 +1,9 @@
+//reference type
+//context
+//instantiation
+
+//[] === []
+//false
+
+//[1] === [1]
+//false
---
+++ array/index.js
@@ -0,0 +1,39 @@
+// array has 2 types
+//static
+//dynamic
+//static array are fixed in size
+
+const strings = ["a", "b", "c", "d"];
+// 4 items takes 4 shells for storing data
+// 4*4 = 16 bytes
+
+//lookup O(1)
+//push O(1)
+//insert O(n)
+//delete O(n)
+
+console.log(strings[2]); //O(1)
+
+//PUSH
+strings.push("e"); //O(1) but can be O(n)
+console.log(strings);
+
+//POP
+strings.pop(); //O(n)
+console.log(strings);
+
+// strings.pop();
+// console.log(strings);
+
+//unshift  (it add items at the begining of the array)
+strings.unshift("z"); //O(n)
+console.log(strings);
+
+//shift  (it remove first item from an array)
+strings.shift();
+console.log(strings);
+
+//splice
+// its used to add item in middle of the array
+strings.splice(2, 0, "alien"); //O(n)
+console.log(strings);
